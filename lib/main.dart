0e6283c8457e59d675f1b90a7e2a92b6b0c71b25
Amine Mersoul <amersoul@e1r6p1.1337.ma>
2018-11-28 18:08:09 +0100

    creating grid view and cards

--- lib/main.dart
+++ lib/main.dart
@@ -115,9 +115,61 @@ class _MyHomePageState extends State<MyHomePage> {
                 Text("VIEW PAST SESSIONS", style: TextStyle(color: Colors.green, fontFamily: 'Quicksand', fontWeight: FontWeight.w900, fontSize: 12.0),)
               ],
             ),
+          ),
+          SizedBox(height: 10.0,),
+          GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 4.0,
+            shrinkWrap: true,
+            children: <Widget>[
+              _buildCard("Tom", "Availabel", 1),
+              _buildCard("Tom", "Away", 2),
+              _buildCard("Tom", "Away", 3),
+              _buildCard("Tom", "Availabel", 4),
+              _buildCard("Tom", "Away", 5),
+              _buildCard("Tom", "Availabel", 6),
+            ],
           )
         ],
       ),
     );
   }
+
+  Widget _buildCard(String name, String status, int cardIndex) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 7.0,
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 12.0,),
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                width: 60.0,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30.0), color: Colors.green, image: DecorationImage(image: NetworkImage("https://pixel.nymag.com/imgs/daily/intelligencer/2017/07/23/23-chris-froome-tdf-winner.w700.h467.2x.jpg"), fit:BoxFit.cover)),
+              )
+            ],
+          ),
+          SizedBox(height: 8.0,),
+          Text(status, style: TextStyle(fontFamily: 'Quicksand', fontWeight: FontWeight.bold, fontSize: 12.0, color: Colors.grey),),
+          SizedBox(height: 15.0,),
+          Expanded(
+            child: Container(
+              width: 175.0,
+              decoration: BoxDecoration(color: status == "Away" ? Colors.grey : Colors.green, borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0))),
+              child: Center(
+                child: Text("Request", style: TextStyle(color: Colors.white, fontFamily: 'Quicksand'),),
+              ),
+            ),
+          )
+        ],
+      ),
+      margin: cardIndex.isEven ? EdgeInsets.fromLTRB(10.0, 0.0, 25.0, 10.0) : EdgeInsets.fromLTRB(25.0, 0.0, 5.0, 10.0),
+    );
+  }
+
 }
